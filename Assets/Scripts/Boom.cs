86c2923a7744c49c1c20566b5c22765dd591b189
Luka28Rad <luka28rad@gmail.com>
2024-11-15 12:52:05 +0100

    Zapravo final

--- Assets/Scripts/Boom.cs
+++ Assets/Scripts/Boom.cs
@@ -29,7 +29,7 @@ public class Boom : NetworkBehaviour
         if (other.CompareTag("Player"))
         {
             other.GetComponent<PlayerManager>().TriggerExplosion();
-            DestroyNetworkObjectServerRpc();
+            Destroy(gameObject);
         }
     }
 }
